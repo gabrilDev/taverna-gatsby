857be8d0baae36ea2f72bb8911b365b584dbc816
gabrilDev <gabriel.gl2302@gmail.com>
2023-01-06 11:56:43 -0300

    chore: tailwind bug fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,8 @@ module.exports = {
   content: [
     "./src/pages/**/*.{js,jsx,ts,tsx}",
     "./src/components/**/*.{js,jsx,ts,tsx}",
+    "./src/partials/**/*.{js,jsx,ts,tsx}",
+    "./src/*/.{js,jsx,ts,tsx}", 
   ],
   theme: {
     extend: {},
