e2ea1460466a3f2ce8d4aff2bd5f9530dc7a5d08
gabrilDev <gabriel.gl2302@gmail.com>
2023-01-09 02:46:46 -0300

    chore: hero and about partials imported to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import type { HeadFC, PageProps } from "gatsby"
 import * as React from "react"
+import About from "../partials/About"
 import Footer from "../partials/Footer"
+import Hero from "../partials/Hero"
 
 const IndexPage: React.FC<PageProps> = () => {
   return (<main>
+    <Hero/>
+    <About/>
     <Footer/>
   </main>
     
