17d366ea896ddff71726ff2be78003af5adc085d
gabrilDev <gabriel.gl2302@gmail.com>
2023-01-09 02:50:36 -0300

    feat:footer partial

--- src/partials/Footer.tsx
+++ src/partials/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="w-full h-[174px] relative bg-gray-800 flex flex-col place-items-center justify-end ">
+    <footer className="w-full h-[174px] relative bg-gray-800 flex flex-col place-items-center  justify-end ">
       <div className="h-3/4  flex flex-col border-t-[3px] w-[1170px] border-white ">
         <p className="text-white text-center my-auto relative">
           ©Copyright. 2021.Todos os direitos reservados.
